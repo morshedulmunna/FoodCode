21a6c812290dcd05fa5ecd4d3e3378cc53164a44
morshedulmunna <morshedulmunna1@gmail.com>
2022-02-22 02:12:07 +0600

    click to change color

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+var header = document.getElementById("eat_time");
+var tagA = header.getElementsByClassName("tagA");
+console.log(tagA);
+
+for (var i = 0; i < tagA.length; i++) {
+  tagA[i].addEventListener("click", function () {
+    //   color Change on click
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    //   color Change on click end
+    //
+    //
+    // Data Added on click
+  });
+}
